3a0a2a3ea90f6d95631f475fdf7a1f27b589b343
vibegpt <cliqueapp@gmail.com>
2025-10-15 14:04:27 +0300

    Fix middleware auth syntax for public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,9 @@ const isPublicRoute = createRouteMatcher([
   "/terms",
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
+export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    auth().protect();
   }
 });
 
